ff46ec7fd18e8d5552c717e612f413a062e24df3
vlad1803 <vlad.somov93@gmail.com>
2017-01-05 23:11:37 +0200

    removed Ligbulb added outside_weather.go

--- outside_weather.go
+++ outside_weather.go
@@ -28,6 +28,7 @@ func main() {
 		Manufacturer: "Vlad Somov",
 	}
 
+	// When creating NewTemperatureSensor you should pass (device info, current temp, min temp, max temp and step value)
 	acc := accessory.NewTemperatureSensor(info, getTemperature(), -100, 100, 0.1)
 	t, err := hc.NewIPTransport(hc.Config{Pin: "YOUR_8_DIGIT_ACCESSORY_PASSWORD"}, acc.Accessory)
 	if err != nil {
